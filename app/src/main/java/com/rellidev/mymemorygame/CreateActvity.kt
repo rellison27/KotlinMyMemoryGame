9e39a6da3a80fb9796578c28c4353cc69fcf8277
Rashaun Ellison <rellison27@gmail.com>
2021-07-31 22:44:58 -0400

    logic for choose image intent

--- app/src/main/java/com/rellidev/mymemorygame/CreateActvity.kt
+++ app/src/main/java/com/rellidev/mymemorygame/CreateActvity.kt
@@ -1,22 +1,37 @@
 package com.rellidev.mymemorygame
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rellidev.mymemorygame.models.BoardSize
 import com.rellidev.mymemorygame.utils.EXTRA_BOARD_SIZE
+import com.rellidev.mymemorygame.utils.isPermissionGranted
+import com.rellidev.mymemorygame.utils.requestPermission
 
 class CreateActvity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "CreateActivity"
+        private const val PICK_PHOTOS_CODE = 655
+        private const val READ_EXTERNAL_PHOTOS_CODE = 248
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
 
     private lateinit var rvImagePicker: RecyclerView
     private lateinit var etGameName: EditText
     private lateinit var btnSave: Button
 
+    private lateinit var adapter: ImagePickerAdapter
     private lateinit var boardSize: BoardSize
     private var numImagesRequired = -1
     private val chosenImageURis = mutableListOf<Uri>()
@@ -35,9 +50,33 @@ class CreateActvity : AppCompatActivity() {
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
 
-        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageURis, boardSize)
+         adapter = ImagePickerAdapter(this, chosenImageURis, boardSize, object: ImagePickerAdapter.ImageClickListener {
+            override fun onPlaceholderClicked() {
+                if (isPermissionGranted(this@CreateActvity, READ_PHOTOS_PERMISSION)) {
+                    launchIntentForPhotos()
+                }else {
+                    requestPermission(this@CreateActvity, READ_PHOTOS_PERMISSION, READ_EXTERNAL_PHOTOS_CODE)
+                }
+            }
+        })
+        rvImagePicker.adapter = adapter
         rvImagePicker.setHasFixedSize(true)
         rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_EXTERNAL_PHOTOS_CODE) {
+            if(grantResults.isNotEmpty()&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            }else {
+                Toast.makeText(this, "In order to create a custom game, you need to provide access to your photos", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -46,5 +85,41 @@ class CreateActvity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode != PICK_PHOTOS_CODE || resultCode != Activity.RESULT_OK || data == null) {
+            Log.w(TAG, "Did not get back data from the launched activity, user likely canceled flow")
+            return
+        }
+        val selectedUri = data.data
+        val clipData = data.clipData
+        if (clipData != null) {
+            Log.i(TAG, "clipData numImages ${clipData.itemCount}: $clipData")
+            for (i in 0 until clipData.itemCount) {
+                val clipItem = clipData.getItemAt(i)
+                if (chosenImageURis.size < numImagesRequired) {
+                    chosenImageURis.add(clipItem.uri)
+                }
+            }
+        } else if (selectedUri != null) {
+            Log.i(TAG, "data: $selectedUri")
+            chosenImageURis.add(selectedUri)
+        }
+        adapter.notifyDataSetChanged()
+        supportActionBar?.title = "Choose pics (${chosenImageURis.size} / $numImagesRequired)"
+        btnSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    private fun shouldEnableSaveButton(): Boolean {
+        return chosenImageURis.size == numImagesRequired
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Choose pics"), PICK_PHOTOS_CODE)
     }
 }
--- app/src/main/java/com/rellidev/mymemorygame/ImagePickerAdapter.kt
+++ app/src/main/java/com/rellidev/mymemorygame/ImagePickerAdapter.kt
@@ -13,9 +13,13 @@ import kotlin.math.min
 class ImagePickerAdapter(
     private val context: Context,
     private val imageUris: List<Uri>,
-    private val boardSize: BoardSize
+    private val boardSize: BoardSize,
+    private val imageClickListener: ImageClickListener
 ) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
 
+    interface ImageClickListener {
+        fun onPlaceholderClicked()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
@@ -51,7 +55,7 @@ class ImagePickerAdapter(
 
         fun bind() {
             ivCustomImage.setOnClickListener{
-
+                imageClickListener.onPlaceholderClicked()
             }
         }
 
